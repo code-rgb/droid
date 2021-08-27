e5561ab0b254580e4e7930cc85c69fda319f2482
Leorio Paradinight <62891774+code-rgb@users.noreply.github.com>
2021-08-27 17:02:19 +0530

    Update vscode_poetry.py

--- vscode_poetry.py
+++ vscode_poetry.py
@@ -5,18 +5,19 @@ from pathlib import Path
 venv_path = subprocess.check_output("poetry env info --path".split())
 venv_path = venv_path.decode("UTF-8")
 
-settings = dict()
-
 Path(".vscode").mkdir(parents=True, exist_ok=True)
-Path(".vscode/settings.json").touch()
+vs_settings = Path(".vscode/settings.json")
+vs_settings.touch()
 
+if vs_settings.isfile():
+    with vs_settings.open("r") as f:
+        settings = json.load(f)
+else:
+    settings = {}
 
-with open(".vscode/settings.json", "r") as f:
-    settings = json.load(f)
-    settings["python.pythonPath"] = venv_path
+settings["python.pythonPath"] = venv_path
 
-with open(".vscode/settings.json", "w") as f:
+with vs_settings.open("w") as f:
     json.dump(settings, f, sort_keys=True, indent=4)
 
-
-print(json.dumps(settings, sort_keys=True, indent=4))
+print(settings)
