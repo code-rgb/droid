2ff7aa85bbfd13a991d68510c013758dd3f2eed0
code-rgb <62891774+code-rgb@users.noreply.github.com>
2021-09-06 08:44:32 +0000

    on push: auto commit

--- droid/modules/anti_arab.py
+++ droid/modules/anti_arab.py
@@ -1,14 +1,14 @@
-import re
-from ..utils import get_media
-from pyrogram import filters
-from pyrogram.client import Client
-from pyrogram.types import Message, User
 import asyncio
-from .. import mod
-from ..core.conversation import Conversation
-from ..decor import OnFlt
-from ..core.command_context import Ctx
+import re
+
+from pyrogram import filters
 from pyrogram.errors import UserAdminInvalid
+from pyrogram.types import User
+
+from .. import mod
+from ..core.command_context import Ctx
+from ..decor import OnFlt
+from ..utils import get_media
 
 CHAT = -1001378211961
 
@@ -19,15 +19,15 @@ class AntiArabic(mod.Module):
         # https://en.wikipedia.org/wiki/Arabic_script#endnote_arabic_alphabet
         self.ARABIC = re.compile(
             "["
-            r"\u0600-\u06FF"  #  Arabic
-            r"\u0750-\u077F"  #  Arabic Supplement
-            r"\u08A0-\u08FF"  #  Arabic Extended-A
-            r"\uFB50-\uFDFF"  #  Arabic Pres. Forms-A
-            r"\uFE70-\uFEFF"  #  Arabic Pres. Forms-B
-            r"\u1EE0-\u1EEF"  #  Arabic Mathematical...
-            r"\u1EC7-\u1ECB"  #  Indic Siyaq Numbers
-            r"\u1ED0-\u1ED4"  #  Ottoman Siyaq Numbers
-            r"\u10E6-\u10E7"  #  Rumi Numeral Symbols
+            r"\u0600-\u06FF"  # Arabic
+            r"\u0750-\u077F"  # Arabic Supplement
+            r"\u08A0-\u08FF"  # Arabic Extended-A
+            r"\uFB50-\uFDFF"  # Arabic Pres. Forms-A
+            r"\uFE70-\uFEFF"  # Arabic Pres. Forms-B
+            r"\u1EE0-\u1EEF"  # Arabic Mathematical...
+            r"\u1EC7-\u1ECB"  # Indic Siyaq Numbers
+            r"\u1ED0-\u1ED4"  # Ottoman Siyaq Numbers
+            r"\u10E6-\u10E7"  # Rumi Numeral Symbols
             "]+"
         )
 
